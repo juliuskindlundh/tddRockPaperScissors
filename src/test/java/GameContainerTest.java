28f8a73837aa4a99602e68d8c4887f894bffd914
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 16:54:17 +0100

    failing test

--- src/test/java/GameContainerTest.java
+++ src/test/java/GameContainerTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +7,11 @@ import org.springframework.util.Assert;
 import rps.GameContainer;
 import rps.Move;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 @SpringBootTest(classes = {GameContainer.class})
@@ -15,7 +19,12 @@ public class GameContainerTest {
 
     @Autowired
     GameContainer gameContainer;
+    Scanner scanner;
 
+    @BeforeEach
+    public void setUp(){
+        scanner = mock(Scanner.class);
+    }
     @Test
     public void test(){
         assertNotNull(gameContainer);
@@ -27,8 +36,21 @@ public class GameContainerTest {
     }
 
     @Test
-    public void process_handle_good_input_test(){
-        fail();
+    public void process_handle_good_input_ROCK_test(){
+        when(scanner.nextLine()).thenReturn("ROCK");
+        assertEquals(Move.ROCK,gameContainer.readPlayerMove(scanner));
+    }
+
+    @Test
+    public void process_handle_good_input_PAPER_test(){
+        when(scanner.nextLine()).thenReturn("PAPER");
+        assertEquals(Move.PAPER,gameContainer.readPlayerMove(scanner));
+    }
+
+    @Test
+    public void process_handle_good_input_SCISSORS_test(){
+        when(scanner.nextLine()).thenReturn("SCISSORS");
+        assertEquals(Move.SCISSORS,gameContainer.readPlayerMove(scanner));
     }
 
     @Test
