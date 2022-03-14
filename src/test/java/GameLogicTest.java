cd891faecb590e044fc603485558ce06548804ce
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 15:46:50 +0100

    failing test

--- src/test/java/GameLogicTest.java
+++ src/test/java/GameLogicTest.java
@@ -4,6 +4,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import rps.GameLogic;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = {GameLogic.class})
 public class GameLogicTest {
@@ -14,5 +15,26 @@ public class GameLogicTest {
     @Test
     public void test(){
         assertNotNull(gameLogic);
+    }
+
+    @Test
+    public void make_move_player_wins_test(){
+        assertTrue(gameLogic.makeMove("ROCK","SCISSORS"));
+        assertTrue(gameLogic.makeMove("PAPER","ROCK"));
+        assertTrue(gameLogic.makeMove("SCISSORS","PAPER"));
+    }
+
+    @Test
+    public void make_move_computer_wins_test(){
+        assertTrue(gameLogic.makeMove("ROCK","PAPER"));
+        assertTrue(gameLogic.makeMove("PAPER","SCISSORS"));
+        assertTrue(gameLogic.makeMove("SCISSORS","ROCK"));
+    }
+
+    @Test
+    public void make_move_draw_test(){
+        assertTrue(gameLogic.makeMove("ROCK","ROCK"));
+        assertTrue(gameLogic.makeMove("PAPER","PAPER"));
+        assertTrue(gameLogic.makeMove("SCISSORS","SCISSORS"));
     }
 }
