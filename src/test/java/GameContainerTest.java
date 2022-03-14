2f48d0e1d2da95b1c522087c9131cfb8e222cf22
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 16:38:50 +0100

    failing test

--- src/test/java/GameContainerTest.java
+++ src/test/java/GameContainerTest.java
@@ -1,9 +1,13 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 import rps.GameContainer;
+import rps.Move;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 @SpringBootTest(classes = {GameContainer.class})
@@ -16,4 +20,20 @@ public class GameContainerTest {
     public void test(){
         assertNotNull(gameContainer);
     }
+
+    @Test
+    public void generate_cpu_move(){
+        Assertions.assertInstanceOf(Move.class,gameContainer.generateCPUMove());
+    }
+
+    @Test
+    public void process_handle__good_input_test(){
+        fail();
+    }
+
+    @Test
+    public void process_handle_bad_input_test(){
+        fail();
+    }
+
 }
