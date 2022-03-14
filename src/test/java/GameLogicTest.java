1e67ba680ce46d7116c81350d25ddf5f6b5b5450
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 15:50:08 +0100

    refactored test

--- src/test/java/GameLogicTest.java
+++ src/test/java/GameLogicTest.java
@@ -3,8 +3,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import rps.GameLogic;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = {GameLogic.class})
 public class GameLogicTest {
@@ -19,22 +18,22 @@ public class GameLogicTest {
 
     @Test
     public void make_move_player_wins_test(){
-        assertTrue(gameLogic.makeMove("ROCK","SCISSORS"));
-        assertTrue(gameLogic.makeMove("PAPER","ROCK"));
-        assertTrue(gameLogic.makeMove("SCISSORS","PAPER"));
+        assertEquals(1,gameLogic.makeMove("ROCK","SCISSORS"));
+        assertEquals(1,gameLogic.makeMove("PAPER","ROCK"));
+        assertEquals(1,gameLogic.makeMove("SCISSORS","PAPER"));
     }
 
     @Test
     public void make_move_computer_wins_test(){
-        assertTrue(gameLogic.makeMove("ROCK","PAPER"));
-        assertTrue(gameLogic.makeMove("PAPER","SCISSORS"));
-        assertTrue(gameLogic.makeMove("SCISSORS","ROCK"));
+        assertEquals(-1,gameLogic.makeMove("ROCK","PAPER"));
+        assertEquals(-1,gameLogic.makeMove("PAPER","SCISSORS"));
+        assertEquals(-1,gameLogic.makeMove("SCISSORS","ROCK"));
     }
 
     @Test
     public void make_move_draw_test(){
-        assertTrue(gameLogic.makeMove("ROCK","ROCK"));
-        assertTrue(gameLogic.makeMove("PAPER","PAPER"));
-        assertTrue(gameLogic.makeMove("SCISSORS","SCISSORS"));
+        assertEquals(0,gameLogic.makeMove("ROCK","ROCK"));
+        assertEquals(0,gameLogic.makeMove("PAPER","PAPER"));
+        assertEquals(0,gameLogic.makeMove("SCISSORS","SCISSORS"));
     }
 }
