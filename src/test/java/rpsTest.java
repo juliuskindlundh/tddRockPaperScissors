822417643826e7cc73d938c8db23ba1dd2775786
Julius Kindlundh <j.kindlundh@gmail.com>
2022-03-14 15:33:27 +0100

    first failing test

---
+++ src/test/java/rpsTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class rpsTest {
+
+    @Autowired
+    GameContainer gameContainer;
+
+    @Test
+    public void test(){
+        assertNotNull(gameContainer);
+    }
+}
